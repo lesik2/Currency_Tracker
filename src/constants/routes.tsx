554e60ed443d66b404ef45b88f37ca1b7e25f9fb
Aliaksei Pukhalski <lesikpuhalskij@gmail.com>
2023-11-28 21:36:13 +0300

    fix: add as const and refactor object Paths_for_link_names

--- src/constants/routes.tsx
+++ src/constants/routes.tsx
@@ -5,19 +5,24 @@ import { Home } from '@pages/Home/index';
 import { Timeline } from '@pages/Timeline/index';
 import { IPaths } from '@customTypes/index';
 
-export const LINKS_NAMES = ['Home', 'Timeline', 'Bank card', 'Contato'];
+export const LINKS_NAMES = ['Home', 'Timeline', 'Bank card', 'Contato'] as const;
 export const PATHS_NAMES = {
   home: '/',
   timeline: '/timeline',
   bankCard: '/bankcard',
   contact: '/contact',
-};
-export const PATHS_FOR_LINK_NAMES = {
-  [PATHS_NAMES.home]: LINKS_NAMES[0],
-  [PATHS_NAMES.timeline]: LINKS_NAMES[1],
-  [PATHS_NAMES.bankCard]: LINKS_NAMES[2],
-  [PATHS_NAMES.contact]: LINKS_NAMES[3],
-};
+} as const;
+export type TypePathForLinkNames = Record<
+  typeof PATHS_NAMES[keyof typeof PATHS_NAMES],
+  typeof LINKS_NAMES[number]
+>
+export const PATHS_FOR_LINK_NAMES: TypePathForLinkNames = Object.values(PATHS_NAMES).reduce(
+  (acc, path, index) => ({
+    ...acc,
+    [path]: LINKS_NAMES[index],
+  }),
+  {} as TypePathForLinkNames,
+);
 export const PATHS = Object.values(PATHS_NAMES);
 export const ROUTES: Readonly<IPaths[]> = [
   {
